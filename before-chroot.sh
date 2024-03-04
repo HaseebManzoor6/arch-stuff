f99138d1d4d8b17a9f8de53d7e9d27660474f6b2
Haseeb <haseebmanzoor6@gmail.com>
2024-03-04 18:45:59 -0500

    (WIP) DE install script

--- before-chroot.sh
+++ before-chroot.sh
@@ -1,7 +1,7 @@
 # Make sure to partition your disk first!
 
-#pacstrap -K /mnt base linux linux-firmware base-devel sudo which git man-db man-pages texinfo neovim most bat zsh networkmanager
-#genfstab -U /mnt >> /mnt/etc/fstab
-#ln -sf /mnt/usr/share/zoneinfo/UTC /mnt/etc/localtime
-#cp -r . ../mnt/archinstall
-#arch-chroot /mnt
+pacstrap -K /mnt $(cat base_packages.txt | tr '\n' ' ')
+genfstab -U /mnt >> /mnt/etc/fstab
+ln -sf /mnt/usr/share/zoneinfo/UTC /mnt/etc/localtime
+cp -r . ../mnt/archinstall
+arch-chroot /mnt
---
+++ desktop-environment.sh
@@ -0,0 +1,11 @@
+pacman -S $(cat de_packages.txt | tr '\n' ' ')
+[[ -d ~/.config ]] || mkdir ~/.config
+cp -r ./gtk ~
+
+# terminal and nvim in Thunar shortcut
+echo "TerminalEmulator=alacritty\nTerminalEmulatorDismissed=true" > /.config/xfce3/helpers.rc
+
+[[ -d ~/.local/share/applications ]] || mkdir -p ~/.local/share/applications
+cp /usr/share/applications/nvim.desktop ~/.local/share/applications/nvim.desktop
+sed -e 's/^Exec=/Exec=exo-open' ~/.local/share/applications/nvim.desktop
+sed -e 's/^Terminal=/Terminal=false' ~/.local/share/applications/nvim.desktop

30d937893e876a7c4f61947386181fcad16c5539
Haseeb <haseebmanzoor6@gmail.com>
2024-03-04 18:48:34 -0500

    (WIP) Desktop Environment install script

--- after-chroot.sh
+++ after-chroot.sh
@@ -1,6 +1,14 @@
 echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
 locale-gen
 echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
-echo "`read -p 'Enter hostname: '`" >> /etc/hostname
+echo 'Enter hostname: '
+read my_hostname
+echo "$my_hostname" >> /etc/hostname
 mkinitcpio -P
+
+# users
 passwd
+groupadd sudo
+echo 'Name the admin user: '
+read admin_username
+useradd -m -G sudo "$admin_username"
